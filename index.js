bee8bdab9f9ec1a0543c180692b4752213a7ef72
rishabram <148410206+rishabram@users.noreply.github.com>
2025-05-19 15:22:19 -0600

    Practiced Map and Splice methods and made my own custom methods with each

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 let years=[];
 console.log(years["2"] !== years["02"]);
 
@@ -48,6 +49,114 @@ Array.prototype.myFilter = function(callbackFn){
 }
 const res2 = array1.myFilter((x)=> x%2 === 1)
     console.log(res2);
+let digits =[1,2,3,4,5]
+/**
+ * @param {number[]} digits
+ * @return {number[]}
+ */
+
+var plusOne = function(digits) {
+    let str = digits.join("")
+    let sum = parseInt(str);
+    sum+=1
+    str = sum.toString()
+    let digArr= str.split('')
+    let arr = digArr.map((str)=>parseInt(str, 10))
+
+    return arr
+};
+res4 = plusOne(digits);
+console.log(res4)
+
+Array.prototype.myMap = function(callbackFn){
+    const mappedArr = []
+    const len = this.length
+    for (let i=0; i<len; i++){
+        if (i in this){
+            if (callbackFn.call(this,this[i],i)){
+                mappedArr.push(callbackFn.call(this,this[i],i))
+            }
+        }
+    }
+    return mappedArr
+}
+console.log(digits.myMap((x)=>x*3))
+
+Array.prototype.mySplice = function(start,end){
+    let splicedArr = []
+    const len = this.length
+    if (start && end) {
+        if (-len <= end && end < 0) {
+            for (let i = start; i <end+len ;i++) {
+                if (i in this) {
+                    splicedArr.push(this[i])
+                }}}
+        else{
+            for (let i = start; i < end; i++) {
+                if (i in this) {
+                    splicedArr.push(this[i])
+                }}}
+        return splicedArr
+    }
+    else if (end) {
+        if (end>0){
+            for (let i = 0; i < end; i++) {
+                if (i in this) {
+                    splicedArr.push(this[i])
+                }}}
+        else if (-len <= end && end < 0) {
+            for (let i = end+len; i <len ;i++) {
+                if (i in this) {
+                    splicedArr.push(this[i])
+                }}}
+        return splicedArr
+    }
+    else if (start) {
+        if (-len <= start && start < 0) {
+            for (let i = start+len; i <len ;i++) {
+                if (i in this) {
+                    splicedArr.push(this[i])
+                }}}
+        else{
+            for (let i = start; i < len; i++) {
+                if (i in this) {
+                    splicedArr.push(this[i])
+                }}}
+        return splicedArr
+    }
+
+    else {
+        for (let i = 0; i < len; i++) {
+            if (i in this) {
+                splicedArr.push(this[i])
+            }}}
+    return splicedArr
+}
+const animals = ["ant", "bison", "camel", "duck", "elephant"];
+
+console.log(animals.mySplice(2));
+// Expected output: Array ["camel", "duck", "elephant"]
+
+console.log(animals.mySplice(2, 4));
+// Expected output: Array ["camel", "duck"]
+
+console.log(animals.mySplice(1, 5));
+// Expected output: Array ["bison", "camel", "duck", "elephant"]
+
+console.log(animals.mySplice(-2));
+// Expected output: Array ["duck", "elephant"]
+
+console.log(animals.mySplice(2, -1));
+// Expected output: Array ["camel", "duck"]
+
+console.log(animals.mySplice());
+// Expected output: Array ["ant", "bison", "camel", "duck", "elephant"]
+
+
+
+
+
+
 
 
 
