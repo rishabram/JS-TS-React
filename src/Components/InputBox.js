60659cb16896dc74c0b7c73944495bfa48e252a5
rishabram <148410206+rishabram@users.noreply.github.com>
2025-05-19 20:55:18 -0600

    Enter on submit event handler

--- src/Components/InputBox.js
+++ src/Components/InputBox.js
@@ -1,13 +1,19 @@
 import React, { useState } from 'react';
-function InputBox({input,setInput}){
+function InputBox({input,setInput, handleAdd}){
     console.log("InputBox component rendering");
 
     const handleInputChange = (e) =>{
         setInput(e.target.value);
     };
+    const handleKeyDown=(e)=>{
+        if (e.key === 'Enter'){
+            handleAdd();
+        }
+    }
+
     return(
         <label>Enter tasks to do!:
-        <input type="text" value={input} onChange={handleInputChange} placeholder="Enter tasks to do!"/>
+        <input type="text" value={input} onChange={handleInputChange} onKeyDown={handleKeyDown} placeholder="Enter tasks to do!"/>
         </label>
     );
 }
