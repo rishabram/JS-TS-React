c3ac302be86bac2e0d3d7b412fd139ade83430b7
rishabram <148410206+rishabram@users.noreply.github.com>
2025-05-21 14:12:23 -0600

    Fixed errors using local variables and useEffect and add temp unit and weather icon

--- WeatherApp/Weather.js
+++ WeatherApp/Weather.js
@@ -6,56 +6,66 @@ function Weather(){
     const [forecastURL, setForecastURL] = useState('');
 
     const [temperature,setTemperature]=useState('')
+    const [weatherURL,setWeatherURL] = useState(``)
+
+
+    const [temperatureUnit,setTemperatureUnit] = useState('');
+    let [icon,setIcon]=useState('')
     const locationURL='http://ip-api.com/json/'
-    let weatherURL = ``
 
 
     async function fetchData(URL){
-        const res = await fetch(URL)
+       try{ const res = await fetch(URL)
         if (!res.ok){
             throw new Error(`Response Error: ${res.status}`)
         }
         const data = await res.json()
-        if (URL === locationURL) {
-            setLongitude(data.lon)
-            setLatitude(data.lat)
-            console.log(data.lat)
-            console.log(data.lon)
-        }
-        else if (URL.includes('https://api.weather.gov/points/')){
-            setForecastURL(data.properties.forecast)
-            console.log(data.properties.forecast)
-            console.log(forecastURL)
-        }
-        else {
-            setTemperature(data.properties.periods[0].temperature)
-            console.log(data.properties.periods[0].temperature)
-            console.log(temperature)
-        }
+            if (URL === locationURL) {
+                setLongitude(data.lon)
+                setLatitude(data.lat)
+                console.log(data.lat)
+                console.log(data.lon)
+            }
+           else if (URL.includes('https://api.weather.gov/points/')){
+                setForecastURL(data.properties.forecast)
+                console.log(data.properties.forecast)
+            }
+           else {
+                setTemperature(data.properties.periods[0].temperature)
+                setTemperatureUnit(data.properties.periods[0].temperatureUnit)
+                setIcon(data.properties.periods[0].icon)
+                console.log(data.properties.periods[0].temperature)
+                console.log(data.properties.periods[0].temperatureUnit)
+                console.log(data.properties.periods[0].icon)
+            }
     }
-    if (latitude !== 0.0 && longitude !== 0.0){
-        weatherURL = `https://api.weather.gov/points/${latitude},${longitude}`
+    catch (error){
+        console.error("Error fetching data:", error, "from URL:", URL)
     }
+    }
+    useEffect(()=>{
+        if (latitude !== 0.0 && longitude !== 0.0){
+            setWeatherURL(`https://api.weather.gov/points/${latitude},${longitude}`);
+        }
+    },[latitude,longitude])
 
     useEffect(()=>{
         if (latitude === 0.0 && longitude === 0.0) {
             fetchData(locationURL)}
-        else if (forecastURL ===''){
+        else if (weatherURL && forecastURL === ''){
             fetchData(weatherURL)
         }
-        else {
+        else if(forecastURL){
             fetchData(forecastURL)
         }
+    },[forecastURL,weatherURL]);
 
-
-
-
-    },[latitude,longitude,forecastURL,weatherURL]);
     return(
         <div>
             <p>Latitude = {latitude}</p>
             <p>Longitude = {longitude}</p>
-            <p>Temperature = {temperature}</p>
+            <p>Temperature = {temperature} {temperatureUnit} {icon && <img src={icon} alt="Weather icon" width="50" />}</p>
+
 
 
         </div>
