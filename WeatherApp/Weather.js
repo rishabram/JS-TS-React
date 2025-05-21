3a634f45eebb4d1a4e24b401eedb2ea800327f99
rishabram <148410206+rishabram@users.noreply.github.com>
2025-05-21 01:08:14 -0600

    Added main functionality of weather app

--- WeatherApp/Weather.js
+++ WeatherApp/Weather.js
@@ -3,22 +3,62 @@ import React, {useEffect, useState} from 'react'
 function Weather(){
     const [longitude,setLongitude] = useState(0.0);
     const [latitude,setLatitude] = useState(0.0);
+    const [forecastURL, setForecastURL] = useState('');
+
+    const [temperature,setTemperature]=useState('')
     const locationURL='http://ip-api.com/json/'
-        async function fetchData(URL){
+    let weatherURL = ``
+
+
+    async function fetchData(URL){
         const res = await fetch(URL)
         if (!res.ok){
-            throw new(`Response Error: ${res.status}`)
+            throw new Error(`Response Error: ${res.status}`)
         }
         const data = await res.json()
-            if (URL === 'http://ip-api.com/json/'){
-                setLongitude(data.lat)
-                setLatitude(data.lon)
-                console.log(data.lat)
-                console.log(data.lon)
-
-            }
-
-
+        if (URL === locationURL) {
+            setLongitude(data.lon)
+            setLatitude(data.lat)
+            console.log(data.lat)
+            console.log(data.lon)
+        }
+        else if (URL.includes('https://api.weather.gov/points/')){
+            setForecastURL(data.properties.forecast)
+            console.log(data.properties.forecast)
+            console.log(forecastURL)
+        }
+        else {
+            setTemperature(data.properties.periods[0].temperature)
+            console.log(data.properties.periods[0].temperature)
+            console.log(temperature)
+        }
     }
+    if (latitude !== 0.0 && longitude !== 0.0){
+        weatherURL = `https://api.weather.gov/points/${latitude},${longitude}`
+    }
+
+    useEffect(()=>{
+        if (latitude === 0.0 && longitude === 0.0) {
+            fetchData(locationURL)}
+        else if (forecastURL ===''){
+            fetchData(weatherURL)
+        }
+        else {
+            fetchData(forecastURL)
+        }
+
+
+
+
+    },[latitude,longitude,forecastURL,weatherURL]);
+    return(
+        <div>
+            <p>Latitude = {latitude}</p>
+            <p>Longitude = {longitude}</p>
+            <p>Temperature = {temperature}</p>
+
+
+        </div>
+    )
 }
 export default Weather;
