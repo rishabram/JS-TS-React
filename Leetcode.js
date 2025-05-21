4915df1cd3278492839b806c53915643ad9cc59b
rishabram <148410206+rishabram@users.noreply.github.com>
2025-05-21 02:09:29 -0600

    Leetcode 2626 - Reducer

--- Leetcode.js
+++ Leetcode.js
@@ -155,6 +155,32 @@ async function sleep(millis) {
     return new Promise((resolve)=>{setTimeout(resolve,millis);})
     
 }
+/** Leetcode 2626. Array Reduce Transformation
+ * @param {number[]} nums
+ * @param {Function} fn
+ * @param {number} init
+ * @return {number}
+ Input: 
+nums = [1,2,3,4]
+fn = function sum(accum, curr) { return accum + curr; }
+init = 0
+Output: 10
+Explanation:
+initially, the value is init=0.
+(0) + nums[0] = 1
+(1) + nums[1] = 3
+(3) + nums[2] = 6
+(6) + nums[3] = 10
+The final answer is 10.
+Solved using a reducer function and using fn param passed through
+ */
+var reduce = function(nums, fn, init) {
+    let total = init
+    for (let i=0;i<nums.length;i++){
+        total = fn(total,nums[i])
+    }
+    return total
+};
 
 
 
