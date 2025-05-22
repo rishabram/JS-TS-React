550e091cf8e55321e487e3ef019b5094df228256
rishabram <148410206+rishabram@users.noreply.github.com>
2025-05-21 18:29:24 -0600

    Promise

--- index.js
+++ index.js
@@ -195,6 +195,20 @@ randomPromise
     .catch((error) => {
         console.error(error);
     });
+//Input a number, double it, increase it by 10, and then multiply by 3.
+// Each operation should be in a separate Promise and then chained together.
+const value = 5;
+const double=(value) => new Promise((resolve)=>{resolve(value*2)
+})
+const addTen = (value)=> new Promise((resolve)=>{resolve(value+10)})
+const multiplyByThree = (value) => new Promise((resolve)=>{resolve(value*3)})
+
+double(value)
+    .then(addTen)
+    .then(multiplyByThree)
+    .then((result) => {
+        console.log(result);
+    });
 
 
 
