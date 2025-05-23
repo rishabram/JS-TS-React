79bfaffeece691dafe092d0f01761a6a8ed77631
rishabram <148410206+rishabram@users.noreply.github.com>
2025-05-23 15:55:33 -0600

    added event handler for deleting item from backend array with fetch

--- node practice/app.js
+++ node practice/app.js
@@ -18,11 +18,22 @@ function App() {
         setItems([...items,data])
         setNewItem("");
     }
+    async function handleDelete(id){
+        const res = await fetch('http://localhost:5000/api/items',{
+            method:'DELETE', headers:{'Content-Type':'application/json'}, body: JSON.stringify({id: id})
+        })
+        const data = await res.json();
+        setItems(data)
+
+    }
     return (
         <div>
             <h1>Items</h1>
             <ul>
-                {items.map((item)=>(<li key={item.id}>{item.name}</li>))}
+                {items.map((item)=>(<li key={item.id}>{item.name}
+                        <button onClick={()=>handleDelete(item.id)}>Delete Item</button>
+                    </li>)
+                    )}
             </ul>
            <form onSubmit={handleSubmit}>
                <input value={newItem}type='text' onChange={(e)=>setNewItem(e.target.value)} placeholder='Add a new item'
