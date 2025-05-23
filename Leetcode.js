f52bdf63518aef6547ee4b5fd7aee4c4a7800dea
rishabram <148410206+rishabram@users.noreply.github.com>
2025-05-23 16:34:50 -0600

    counter

--- Leetcode.js
+++ Leetcode.js
@@ -262,5 +262,32 @@ var fibGenerator = function*() {
  * gen.next().value; // 1
  */
 
+/** Leetcode 2665. Counter II
+ * @param {integer} init
+ * @return { increment: Function, decrement: Function, reset: Function }
+ */
+var createCounter = function(init) {
+    let start = init
+    return {
+    increment:()=>
+         ++start,
+    
+    decrement:()=>
+        --start,
+    
+    reset:()=>
+        start = init,
+    }
+
+    
+};
+
+/** Leetcode 2665. Counter II
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */
+
 
 
