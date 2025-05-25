5d97dc234dab4599918e3b7a7917c4330a1e2fec
rishabram <148410206+rishabram@users.noreply.github.com>
2025-05-25 13:59:28 -0600

    Made my own wrapper class

--- Leetcode.js
+++ Leetcode.js
@@ -300,6 +300,40 @@ var chunk = function(arr, size) {
         }   
     return answer
 };
+/**Leetcode 2695. Array Wrapper
+ * @param {number[]} nums
+ * @return {void}
+ */
+var ArrayWrapper = function(nums) {
+    this.nums=nums
+};
+
+/**
+ * @return {number}
+ */
+ArrayWrapper.prototype.valueOf = function() {
+    let total=0
+    for (let i = 0;i<this.nums.length;i++){
+       total+=this.nums[i]
+    }
+    return total
+}
+
+/**
+ * @return {string}
+ */
+ArrayWrapper.prototype.toString = function() {
+   return "["+this.nums.toString()+"]"
+    
+}
+
+/**
+ * const obj1 = new ArrayWrapper([1,2]);
+ * const obj2 = new ArrayWrapper([3,4]);
+ * obj1 + obj2; // 10
+ * String(obj1); // "[1,2]"
+ * String(obj2); // "[3,4]"
+ */
 
 
 
