7c39fd9627e7f15935dd8c1d64097fa439de730b
rishabram <148410206+rishabram@users.noreply.github.com>
2025-05-26 17:47:19 -0600

    Learnt about the cancelTimeout function in window and how you need to pass in the timer id to use it but i needed a little help on this one

--- Leetcode.js
+++ Leetcode.js
@@ -364,9 +364,50 @@ var expect = function(val) {
 
 
 
-/**
+/**Leetcode 2715. Timeout Cancellation
  * expect(5).toBe(5); // true
  * expect(5).notToBe(5); // throws "Equal"
+ */
+/**
+ * @param {Function} fn
+ * @param {Array} args
+ * @param {number} t
+ * @return {Function}
+ Learnt about the cancelTimeout function in window and how you need to pass in the timer id to use it but i needed a little help on this one
+ */
+var cancellable = function(fn, args, t) {
+
+let timer = setTimeout(()=>{
+    fn(...args)
+
+    
+},t);
+return function(){
+    clearTimeout(timer);
+}
+}
+/**
+ *  const result = [];
+ *
+ *  const fn = (x) => x * 5;
+ *  const args = [2], t = 20, cancelTimeMs = 50;
+ *
+ *  const start = performance.now();
+ *
+ *  const log = (...argsArr) => {
+ *      const diff = Math.floor(performance.now() - start);
+ *      result.push({"time": diff, "returned": fn(...argsArr)});
+ *  }
+ *       
+ *  const cancel = cancellable(log, args, t);
+ *
+ *  const maxT = Math.max(t, cancelTimeMs);
+ *           
+ *  setTimeout(cancel, cancelTimeMs);
+ *
+ *  setTimeout(() => {
+ *      console.log(result); // [{"time":20,"returned":10}]
+ *  }, maxT + 15)
  */
 
 
