93f40999de6396ee42c5c12e0c74c8fa9fc0502b
rishabram <148410206+rishabram@users.noreply.github.com>
2025-06-03 14:14:08 -0600

    Added FindLast method similar to find() but finds the element from reverse

--- arraymethods.js
+++ arraymethods.js
@@ -363,6 +363,43 @@ function isPrime(element) {
 
 console.log([4, 6, 8, 9, 12].myFindIndex(isPrime)); // -1, not found
 console.log([4, 6, 7, 9, 12].myFindIndex(isPrime)); // 2 (array[2] is 7)*/
+Array.prototype.myFindLast=function(fn){
+    len=this.length
+    for (let i=len-1;i>=0;i--){
+        if (i in this){
+
+            if(fn.call(this,this[i],i)){
+
+                return this[i];
+            }
+
+        }
+    }
+    return undefined
+}/*
+const array1 = [5, 12, 50, 130, 44];
+const found = array1.findLast((element) => element > 45);
+console.log(found);
+const inventory = [
+    { name: "apples", quantity: 2 },
+    { name: "bananas", quantity: 0 },
+    { name: "fish", quantity: 1 },
+    { name: "cherries", quantity: 5 },
+];
+const result = inventory.findLast(({ quantity }) => quantity < 2);
+console.log(result);
+// { name: "fish", quantity: 1 }
+function isPrime(element) {
+    if (element % 2 === 0 || element < 2) {
+        return false;}
+    for (let factor = 3; factor <= Math.sqrt(element); factor += 2) {
+        if (element % factor === 0) {
+            return false;
+        }
+    }
+    return true;}
+console.log([4, 6, 8, 12].findLast(isPrime)); // undefined, not found
+console.log([4, 5, 7, 8, 9, 11, 12].findLast(isPrime)); // 11*/
 
 
 
