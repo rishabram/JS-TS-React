afec162002e86285c2515504065cfd987e48af69
rishabram <148410206+rishabram@users.noreply.github.com>
2025-06-05 12:03:52 -0600

    practiced javascript linkedlist implementation

--- arraymethods.js
+++ arraymethods.js
@@ -554,6 +554,30 @@ ratings.myForEach(async (rating) => {
 });
 
 console.log(sum);*/
+/** Leetcode - 83. Remove Duplicates from Sorted List
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var deleteDuplicates = function(head) {
+    let res=head;
+  
+   while (head && head.next){
+    if (head.val === head.next.val){
+        head.next=head.next.next
+    } 
+    else{
+        head=head.next
+    }
+   }
+    return res
+};
 
 
 
