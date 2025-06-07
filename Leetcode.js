9936dbeab7d7f5c7e8f3d8dd1c5681e81b7983a7
rishabram <148410206+rishabram@users.noreply.github.com>
2025-06-07 17:28:08 -0600

    Refreshed my tree traversal logic and finished in order traversal

--- Leetcode.js
+++ Leetcode.js
@@ -621,5 +621,31 @@ var maxProfit = function(prices) {
     return maxProfit
     
 };
+/** Leetcode 94. Binary Tree Inorder Traversal
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var inorderTraversal = function(root) {
+    const res=[]
+    function inorder(node){
+        if (!node){
+            return
+        }
+        inorder(node.left)
+        res.push(node.val)
+        inorder(node.right)
 
+    }
+    inorder(root)
+    return res
+    
+};
 
