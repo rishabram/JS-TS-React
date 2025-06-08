3bdb557d59473259dc340be109ea749be8fc6d14
rishabram <148410206+rishabram@users.noreply.github.com>
2025-06-07 18:00:20 -0600

    sametree recursive

--- Leetcode.js
+++ Leetcode.js
@@ -647,5 +647,32 @@ var inorderTraversal = function(root) {
     inorder(root)
     return res
     
+}; 
+/** Leetcode 94. Binary Tree Inorder Traversal
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var inorderTraversal = function(root) {
+    const res=[]
+    function inorder(node){
+        if (!node){
+            return
+        }
+        inorder(node.left)
+        res.push(node.val)
+        inorder(node.right)
+
+    }
+    inorder(root)
+    return res
+    
 };
 
