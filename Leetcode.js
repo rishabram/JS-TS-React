0b8c4f6a94acff41fbccad6b7e3e742fa8670ff4
rishabram <148410206+rishabram@users.noreply.github.com>
2025-06-09 12:49:54 -0600

    Finished max depth recursively

--- Leetcode.js
+++ Leetcode.js
@@ -696,3 +696,23 @@ var isSymmetric = function(root) {
     }
     return mirror(root.right,root.left)
 };
+/** Leetcode Max Depth
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var maxDepth = function(root) {
+    if (root===null){
+        return 0
+    }
+    else{
+        return 1 + Math.max(maxDepth(root.left),maxDepth(root.right))
+    }
+};
